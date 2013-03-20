a6f7f3411d0e18b3ac89129c55e2d9896db4239b
theHerb <msh.cal.ab@gmail.com>
2013-03-19 20:36:18 -0600

    uploader fully functional

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
-    #:region                 => 'us-east-1'
+    :region                 => 'us-east-1'
   }
 
   config.fog_directory = ENV["AWS_S3_BUCKET"]

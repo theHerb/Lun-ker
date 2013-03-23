2676a27c4c2cb1a2b1371283b38f9f64136feb64
theHerb <msh.cal.ab@gmail.com>
2013-03-23 10:26:43 -0600

    Delayed email

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -32,11 +32,11 @@ class UserFriendship < ActiveRecord::Base
 	end
 
 	def send_request_email 
-		UserNotifier.friend_requested(id).deliver
+		UserNotifier.delay_for(5.seconds).friend_requested(id)
 	end
 
 	def send_acceptance_email 
-		UserNotifier.friend_request_accepted(id).deliver
+		UserNotifier.delay_for(5.seconds).friend_request_accepted(id)
 	end
 
 	def mutual_friendship 

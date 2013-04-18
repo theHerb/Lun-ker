879c6a1b9850e87eb11189b322b6b46b3d2fb85c
Matt Hartwig <msh.cal.ab@gmail.com>
2013-04-18 11:55:44 -0600

    styling

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,16 +22,12 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [100, 100]
   end
 
-  version :home do
-    process :resize_to_limit => [300, 300]
-  end
-
   version :photo_fill do
     process :resize_to_fill => [200, 200]
   end
 
   version :photo_limit do
-    process :resize_to_fill => [200, 200]
+    process :resize_to_limit => [200, 200]
   end
 
   version :large do
